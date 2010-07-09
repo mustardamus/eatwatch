db1101184d8d304359370f47bf7de6c98b0e0afd
Sebastian Senf <contact@mustardamus.com>
2010-07-09 05:47:31 +0200

    gimme some break

--- eatwatch.rb
+++ eatwatch.rb
@@ -3,5 +3,23 @@ get "/" do
 end
 
 get "/eaten" do
-  "text" #read from file
+  File.open('counters/eaten.txt').read.to_s
+end
+
+post "/eaten" do
+  if numeric?(params[:value])
+    current = File.open('counters/eaten.txt').read.to_i
+    file = File.open('counters/eaten.txt', 'w')
+    file.write(current + params[:value].to_i)
+    file.close
+    "done"
+  else
+    "error"
+  end
+end
+
+private
+
+def numeric?(object)
+  true if Float(object) rescue false
 end
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
   $.jQTouch(); //http://stackoverflow.com/questions/2543397/how-to-load-jqtouch-on-demand to get it to work
+  
+  
+  $('#calories-submit .eaten').click(function() {
+    var el = $(this);
+    var calories = $('#calories');
+    
+    if(calories.val().length) {
+      el.addClass('active');
+      
+      $.post('/eaten', { value: calories.val() }, function(data) {
+        console.log(data);
+        setTimeout(function() { //show minimal progress on fast server
+          el.removeClass('active');
+          calories.val('');
+        }, 1000);
+      });
+    }
+    
+    return false;
+  });
 });
